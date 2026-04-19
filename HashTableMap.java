b167c2a960b5adac8e40716c1e7dad323a991d0e
FayzaanV <fayzaanvirk6@yahoo.com>
2026-04-18 22:03:33 -0500

    Added comments to constructors for midweek deadline

--- HashTableMap.java
+++ HashTableMap.java
@@ -3,19 +3,30 @@ import java.util.NoSuchElementException;
 
 public class HashTableMap<KeyType, ValueType> implements MapADT<KeyType, ValueType> {
     
+    // Array to store the table. Linked Lists of Pair objects used to handle collisions
     LinkedList<Pair>[] table = null;
 
+    /**
+     * This constructor takes one parameter and sets the initial size of the HashTable to that
+     * @param capacity is the initial size
+     * @throws IllegalArgumentException if the capacity parameter is zero or negative
+     */
     @SuppressWarnings("unchecked")
     public HashTableMap(int capacity) {
         if (capacity < 1) {
             throw new IllegalArgumentException("Capacity must be at least one");
         }
 
+        // Cast the array to hold LinkedList<Pair> ojects since it can't be declared to do so directly.
         table = (LinkedList<Pair>[]) new LinkedList[capacity];
     }
 
+    /**
+     * This constructor takes no parameters and set the HashTable to an initial size of 8
+     */
     @SuppressWarnings("unchecked")
     public HashTableMap() {
+        // Cast the array to hold LinkedList<Pair> ojects since it can't be declared to do so directly.
         table = (LinkedList<Pair>[]) new LinkedList[8];
     }
 
